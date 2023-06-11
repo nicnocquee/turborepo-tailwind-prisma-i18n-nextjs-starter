a3f26b73aa557edddfe9ce1338cf79a5bc2a0136
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-06-11 18:28:36 +0200

    fix middleware

--- packages/i18n/src/middleware/index.ts
+++ packages/i18n/src/middleware/index.ts
@@ -14,15 +14,12 @@ const cookieName = "i18next";
 export function middleware(req: NextRequest) {
   let lng;
   if (req.cookies.has(cookieName)) {
-    console.log(`middlware: has cookie`);
     lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
   }
   if (!lng) {
-    console.log(`no cookie, try headers`);
     lng = acceptLanguage.get(req.headers.get("Accept-Language"));
   }
   if (!lng) {
-    console.log(`no in headers, fallback lang`);
     lng = fallbackLng;
   }
 
@@ -31,27 +28,22 @@ export function middleware(req: NextRequest) {
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith("/_next")
   ) {
-    console.log(`the lang in param is not supported, redirecting to ${lng}`);
     return NextResponse.redirect(
       new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
     );
   }
 
   if (req.headers.has("referer")) {
-    console.log(`has referer`);
     const refererUrl = new URL(req.headers.get("referer")!);
     const lngInReferer = languages.find((l) =>
       refererUrl.pathname.startsWith(`/${l}`)
     );
     const response = NextResponse.next();
-    console.log(`lngInReferer is ${lngInReferer}`);
     if (lngInReferer) {
       response.cookies.set(cookieName, lngInReferer);
     }
     return response;
   }
-
-  console.log(`continue as normal ${lng}`);
 
   return NextResponse.next();
 }
