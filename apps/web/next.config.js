fb77ddb12bec07adadd560c0fe39985c7718c0da
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-06-11 16:06:11 +0200

    try redirect

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,15 @@ const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/de",
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
