162e6701ba898bc93f357033a1decf48eacab792
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-06-15 23:16:55 +0200

    revert

--- apps/web/src/app/[lng]/components/Footer/client.tsx
+++ apps/web/src/app/[lng]/components/Footer/client.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import { FooterBase } from "./FooterBase";
 import { useTranslation } from "i18n/client";
 
 export const Footer = ({ lng }) => {
+  return (
+    <Suspense fallback="loading">
+      <FooterWithTranslation lng={lng} />
+    </Suspense>
+  );
+};
+
+const FooterWithTranslation = ({ lng }) => {
   const { t } = useTranslation(lng, "footer");
   return <FooterBase t={t} lng={lng} />;
 };
